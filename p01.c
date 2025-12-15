a631291e9e4be506f925d23754149e5fdbc0955a
Kushal Sharma <kushalrakesh019@gmail.com>
2025-12-15 17:21:33 +0530

    Add files via upload

--- p01.c
+++ p01.c
@@ -11,21 +11,25 @@ int main()
     };
 
     struct student *st;
-
+    st->id=0;
+    int i=1;
+    // printf("......%d.....\n", i);
     scanf("%d", &st->id);
-    int i=0;
+
     while (st->id != -1)
     {
-        printf("......%d.....\n", i);
         scanf("%d", &st->Maths);
         scanf("%d", &st->Science);
         printf("%d %d %d\n" ,st->id, st->Maths, st->Science);
-        printf("%d %d %d", &st->id, &st->Maths, &st->Science);
+        printf("%d %d %d %d\n", &st->id, &st->Maths, &st->Science, st->next);
 
         st= st->next;
+        i++;
+        // printf("......%d.....\n", i);
 
-        printf("%d %d %d", &st->id, &st->Maths, &st->Science);
+        printf("%d %d %d\n", &st->id, &st->Maths, &st->Science);
 
+        scanf("%d", &st->id);
     }
     
 
